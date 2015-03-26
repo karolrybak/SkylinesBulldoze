b2c9e4043e27ac894c45fed247030fa5bcf5d199
Karol Rybak <karolrybak@gmail.com>
2015-03-26 17:40:18 +0100

    Bulldozing trees work

---
+++ SkylinesBulldoze/Mod.cs
@@ -0,0 +1,32 @@
+﻿using ColossalFramework;
+using ColossalFramework.UI;
+using ICities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkylinesBulldoze
+{
+    public class Mod : IUserMod
+    {
+        public string Description
+        {
+            get { return "Destroy!!! 1."; }
+        }
+
+        public string Name
+        {
+            get { return "Destroy Tool"; }
+        }
+    }
+    public class LoadingExtension : LoadingExtensionBase
+    {
+        public BetterBulldozeTool bulldozeTool;
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            GameObject gameController = GameObject.FindWithTag("GameController");
+            bulldozeTool = gameController.AddComponent<BetterBulldozeTool>();
+        }
+    }
+}
